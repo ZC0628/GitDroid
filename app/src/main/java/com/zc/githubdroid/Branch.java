dc6dbbe7d347beb5a4594656c0bea61df588d3a7
ZC0628 <546625576@qq.com>
2016-08-27 18:59:23 +0800

    First branch

---
+++ app/src/main/java/com/zc/githubdroid/Branch.java
@@ -0,0 +1,22 @@
+package com.zc.githubdroid;
+
+/**
+ * Created by Administrator on 2016/8/27.
+ */
+public class Branch {
+
+
+    /**
+     *
+     * safsdfs
+     * fsdfds
+     * sdfsdf
+     * asdfsdfsdfsdgfgukeegh
+     *
+     *
+     *
+     * dhjkuewrwefasdgdhj
+     * dfsdfasgtykukjhgdsqwehjgfbnmncx
+     *   不知道哈
+     */
+}
