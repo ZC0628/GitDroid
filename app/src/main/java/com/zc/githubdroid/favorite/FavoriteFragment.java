dfe1f28945950cf95e415a8c5bae5fe4745aaac8
ZC0628 <546625576@qq.com>
2016-09-02 10:55:52 +0800

    收藏页面，上下文菜单的视图

--- app/src/main/java/com/zc/githubdroid/favorite/FavoriteFragment.java
+++ app/src/main/java/com/zc/githubdroid/favorite/FavoriteFragment.java
@@ -3,9 +3,12 @@ package com.zc.githubdroid.favorite;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
@@ -62,8 +65,15 @@ public class FavoriteFragment extends Fragment implements PopupMenu.OnMenuItemCl
 
         //默认显示的是全部的数据
         setData(R.id.repo_group_all);
+
+        //注册上下文菜单——表明我们的菜单是作用到Listview身上的
+        registerForContextMenu(listView);
+
+
+
     }
 
+    //弹出式菜单
     @OnClick(R.id.btnFilter)
     public void showPopupMenu(View view){
         PopupMenu popupMenu = new PopupMenu(getContext(),view);
@@ -119,5 +129,50 @@ public class FavoriteFragment extends Fragment implements PopupMenu.OnMenuItemCl
                 adapter.setData(localRepoDao.queryForGroupId(currentRepoGroupId));
                 break;
         }
+    }
+
+    /**
+     * 上下文菜单ContextMenu——作用到谁身上弹出的菜单
+     *1.表明我们作用到谁身上...作用到Listview上
+     *2.创建出上下文菜单——移动至，删除
+     *3.监听点击的菜单哪一项
+     *4.
+     */
+    /*
+    ContextMenu menu——上下文菜单
+    View v——作用到谁身上的
+    ContextMenu.ContextMenuInfo menuInfo——上下文菜单的基本详细信息
+     */
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        //是不是展示在Listview上
+        if(v.getId() == R.id.listView){
+            //用菜单填充器将menu填充到ContextMenu
+            MenuInflater menuInflater = getActivity().getMenuInflater();//得到菜单填充器
+            menuInflater.inflate(R.menu.menu_context_favorite,menu);//menu,填充到上下文菜单上
+
+        //将我们数据库类别表里面的类别数据填充到移动至的子菜单里面
+            SubMenu subMenu = menu.findItem(R.id.sub_menu_move).getSubMenu();//找到移动至菜单并获得子菜单
+        //仓库类别表查询所有的类别信息
+            List<RepoGroup> repoGroups = repoGroupDao.queryForAll();
+            //利用增强For循环去添加子菜单
+            for (RepoGroup repogroup:repoGroups) {
+                subMenu.add(R.id.menu_group_move,repogroup.getId(),Menu.NONE,repogroup.getName());
+            }
+
+
+
+
+        }
+    }
+
+    //监听点击的菜单哪一项
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+
+
+        return super.onContextItemSelected(item);
     }
 }
